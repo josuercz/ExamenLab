d8cea8dd7be4bebb687d838819c7b90ea01a0926
Nickyi175 <124185028+Nickyi175@users.noreply.github.com>
2023-08-07 12:22:29 -0600

    hola

---
+++ BlockBuster/src/Prueba1/BlockBusterItem.java
@@ -0,0 +1,23 @@
+package Prueba1;
+
+import java.util.Calendar;
+
+public abstract class BlockBusterItem {
+    protected String nombre;
+    protected int codigo;
+    protected double precioRenta;
+    protected Calendar fecha;
+
+    public BlockBusterItem(String nombre, int codigo, double precioRenta) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.precioRenta = precioRenta;
+        fecha=Calendar.getInstance();
+    }
+    
+    public String toString() {
+        return "Nombre: "+nombre+", Código: "+codigo+", Precio de renta: Lps. "+precioRenta;
+    }
+    public abstract double pagoRenta(int dias); 
+    
+}
