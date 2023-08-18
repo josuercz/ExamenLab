abf357e422f501e936ef80d9dc9520e0ff8e272c
Ana Reyes <Ana Reyes@DESKTOP-BOOD2DP>
2023-08-18 14:52:09 -0600

    mm

---
+++ src/examenlab/SocialClass.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examenlab;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Ana Reyes
+ */
+public class SocialClass {
+
+    private List<String> friends;
+    private List<String> posts;
+    private String username;
+
+    public SocialClass(String username) {
+        this.friends = new ArrayList<>();
+        this.posts = new ArrayList<>();
+        this.username = username;
+    }
+
+    public boolean addFriends(String user) {
+        if (!friends.contains(user) && user != null && !user.equals(this.username)) {
+            friends.add(user);
+            return true;
+        } else {
+            return false;
+
+        }
+    }
+
+    public void addPost(String msg) {
+        posts.add(msg);
+
+    }
+
+    public abstract void timeline();
+
+    public void myProfile() {
+        System.out.println("Username: " + username);
+        System.out.print("");
+        timeline();
+        System.out.println("Friends: ");
+        int ami = 0;
+        for (String friend : friends) {
+            System.out.println(friend + " ");
+            ami++;
+
+            if (ami >= 10) {
+                System.out.println();
+                ami =0;
+            }
+
+        }
+        System.out.println();
+    }
+}
