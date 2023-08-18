8279fcdf829cd730eacc925a098b94a0c1c5163d
josuc <josuc@Josue>
2023-08-18 15:22:32 -0600

    UberSocial

---
+++ src/examenlab/UberSocial.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examenlab;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author josuc
+ */
+public class UberSocial {
+    ArrayList <SocialClass> Cuentas;
+    
+    public UberSocial() {
+        Cuentas = new ArrayList<>();
+    }
+    
+    public SocialClass buscar(String username) {
+         for (SocialClass cuentas : Cuentas) {
+            if (cuentas.username.equals(username)) {
+                return cuentas;
+            }
+        }
+        return null;
+    }
+    
+    public void agregarCuenta(String username, String tipo) {
+        SocialClass account = null;
+        if (tipo.equals("FACEBOOK")) {
+            account = new Facebook(username) {};
+        } else if (tipo.equals("TWITTER")) {
+            //account = new Twitter(username);
+        }
+        
+        if (account != null) {
+            Cuentas.add(account);
+        }
+    }
+    
+    public void agregarPost(String username, String post) {
+        SocialClass cuenta = buscar(username);
+        
+        if (cuenta != null) {
+            cuenta.addPost(post);
+        }
+    }
+    
+    public void agregarAmigo(String user1, String user2) {
+        SocialClass cuenta1 = buscar(user1);
+        SocialClass cuenta2 = buscar(user2);
+        
+        if (cuenta1 != null && cuenta2 != null && cuenta1.getClass().equals(cuenta2.getClass())) {
+            ((Facebook) cuenta1).addFriends(user2);
+            ((Facebook) cuenta2).addFriends(user1);
+        }
+    }
+    
+    public void agregarComment(String username, int postID, String autor, String comentario) {
+        SocialClass cuenta = buscar(username);
+        
+        if (cuenta instanceof Facebook) {
+            Comment nuevoComentario = new Comment(postID, autor, comentario);
+            ((Facebook) cuenta).addComment(nuevoComentario);
+        }
+    }
+    
+    
+    public void profileForm(String username) {
+        
+    }
+}
